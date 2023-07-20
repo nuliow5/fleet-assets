b48f4432208ab4337a389d015051b9041845d6c2
nuliows <nuliows@gmail.com>
2023-07-20 21:35:36 +0300

    fix bug: now we cant edit sim card with unxist truck id ir license plate

--- src/main/java/lt/gerasimovas/fleetassets/repositories/TruckRepository.java
+++ src/main/java/lt/gerasimovas/fleetassets/repositories/TruckRepository.java
@@ -4,4 +4,5 @@ import lt.gerasimovas.fleetassets.entities.Truck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TruckRepository extends JpaRepository<Truck, Long> {
+    Truck findByLicensePlate(String licensePlate);
 }
