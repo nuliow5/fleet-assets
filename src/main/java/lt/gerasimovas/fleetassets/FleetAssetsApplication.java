d26b7ec556f39b83d12b72fd4dbe5164e5b74a2e
nuliows <nuliows@gmail.com>
2023-08-07 21:39:06 +0300

    finish jwtToken

--- src/main/java/lt/gerasimovas/fleetassets/FleetAssetsApplication.java
+++ src/main/java/lt/gerasimovas/fleetassets/FleetAssetsApplication.java
@@ -2,7 +2,9 @@ package lt.gerasimovas.fleetassets;
 
 import lt.gerasimovas.fleetassets.auth.AuthenticationService;
 import lt.gerasimovas.fleetassets.auth.RegisterRequest;
+import lt.gerasimovas.fleetassets.repositories.UserEntityRepository;
 import org.hibernate.event.spi.SaveOrUpdateEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,36 +15,41 @@ import static lt.gerasimovas.fleetassets.enumes.Role.MANAGER;
 
 @SpringBootApplication
 public class FleetAssetsApplication {
+    @Autowired
+    UserEntityRepository userEntityRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FleetAssetsApplication.class, args);
-		System.out.println("FLEET_ASSETS start");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FleetAssetsApplication.class, args);
+        System.out.println("FLEET_ASSETS start");
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service
-	) {
-		return args -> {
-			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("admin@mail.com")
-					.password("password")
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        if (userEntityRepository.findAll().isEmpty()) {
+            return args -> {
+                var admin = RegisterRequest.builder()
+                        .firstname("Admin")
+                        .lastname("Admin")
+                        .email("admin@mail.com")
+                        .password("admin")
+                        .role(ADMIN)
+                        .build();
+                System.out.println("Admin token: " + service.register(admin).getAccessToken());
 
-			var manager = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("manager@mail.com")
-					.password("password")
-					.role(MANAGER)
-					.build();
-			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+                var manager = RegisterRequest.builder()
+                        .firstname("Manager")
+                        .lastname("Manager")
+                        .email("manager@mail.com")
+                        .password("manager")
+                        .role(MANAGER)
+                        .build();
+                System.out.println("Manager token: " + service.register(manager).getAccessToken());
 
-		};
-	}
+            };
+        }
+        return null;
+    }
 
 }
